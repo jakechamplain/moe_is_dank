ef8a5df0571dc28057ac9721f673e819db022a21
Jaroslav Fedorčák <jaroslav.fedorcak@yahoo.com>
2018-03-15 13:05:18 +0000

    dummy c program that gets executed from docker when calling ./verify.sh

---
+++ proj-skeleton/pipair.c
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <iterator>
+#include <cstdlib>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <utility>
+#include <stdio.h>
+
+int main (int argc, char* argv[]) {
+	int support = atoi(argv[1]);
+	int confidence = atoi(argv[2]);
+	printf("%d",support);
+	printf("%d", confidence);
+	return 0;
+}
