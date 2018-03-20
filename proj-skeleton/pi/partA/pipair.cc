7ac958f076c78a4356b8b41cd9dc6850fa1d52f5
Jaroslav Fedorčák <jaroslav.fedorcak@yahoo.com>
2018-03-20 17:19:20 +0000

    partA is probably done

--- proj-skeleton/pi/partA/pipair.cc
+++ proj-skeleton/pi/partA/pipair.cc
@@ -49,12 +49,43 @@ string hashIntToString (int i){
 	}	
 }
 
+void lookForBugs(CallGraph callGraph,int firstMethod,int secondMethod,int support,double confidence){
+	//calculate support for individual methods and pairs
+	for(CallGraph::iterator it = callGraph.begin(); it!= callGraph.end(); ++it){	
+		//we go through sets of methods of every scope
+		int scope = it->first;
+		set<int> scopeMethods = it->second;
+		if(scopeMethods.find(firstMethod) != scopeMethods.end() //first method is in this scope 
+			&& scopeMethods.find(secondMethod) == scopeMethods.end()) //second method is not in this scope
+			{
+				string firstMethodString = hashIntToString(firstMethod).c_str();
+				string secondMethodString = hashIntToString(secondMethod).c_str();
+				
+				if (firstMethodString>secondMethodString){
+					string help = firstMethodString;
+					firstMethodString = secondMethodString;
+					secondMethodString = help;
+				}
+				
+				printf("bug: %s in %s, pair: (%s, %s), support: %d, confidence: %.2f%%\n",
+					hashIntToString(firstMethod).c_str(),
+					hashIntToString(scope).c_str(),
+					firstMethodString.c_str(),
+					secondMethodString.c_str(),
+					support, 
+					confidence);
+				
+			}
+		
+	}
+}
+
 int main (int argc, char* argv[]) {
 	
 	int support = atoi(argv[1]);
-	int confidence = atoi(argv[2]);
+	double confidence = atoi(argv[2]);
 	//printf("Support is %d",support);
-	//printf("Confidence is %d", confidence);
+	//printf("Confidence is %f", confidence);
 	CallGraph callGraph;
 	
 	string line;
@@ -88,9 +119,9 @@ int main (int argc, char* argv[]) {
 		if(words.size() == 7){
 			//std::cout << words[5];
 			scope = words[5];
-			size_t first = scope.find_first_of('\'');
-			size_t last = scope.find_last_of('\'');
-			scope = scope.substr(first+1, last-first-1);
+			size_t firstQuote = scope.find_first_of('\'');
+			size_t lastQuote = scope.find_last_of('\'');
+			scope = scope.substr(firstQuote+1, lastQuote-firstQuote-1);
 			scopeInt = hashStringToInt(scope);
 			//std::cout << scope;
 			//printf("%s\n",hashIntToString(scopeInt).c_str());
@@ -100,17 +131,23 @@ int main (int argc, char* argv[]) {
 			if(words[2] == "external" && words[3] == "scope") {
 			  continue;
 			}
+			if(words[2] == "external" && words[3] == "node") {
+			  continue;
+			}			
 			// we don't care about the root
 			if (words[1] == "Root" || scope == "") {
 			  continue;
 			}
 			string method = words[3];
+			size_t firstQuote = method.find_first_of('\'');
+			size_t lastQuote = method.find_last_of('\'');
+			method = method.substr(firstQuote+1, lastQuote-firstQuote-1);
 			int methodInt = hashStringToInt(method);
 			//std::cout << methodInt;
 			
 			// add scope and method to callgraph
 			callGraph[scopeInt].insert(methodInt);
-			//printf("%s, %s\n",scope.c_str(), hashIntToString(methodInt).c_str());
+			//printf("inserting into callgraph: %s, %s\n",scope.c_str(), hashIntToString(methodInt).c_str());
 		}
 		//std::cout << line// << std::endl;//outprint line to test it
 		
@@ -119,9 +156,11 @@ int main (int argc, char* argv[]) {
 	
 	
 	
-	CallGraph::iterator it = callGraph.begin();
+	//CallGraph::iterator it = callGraph.begin();
 	//calculate support for individual methods and pairs
-	while(it != callGraph.end()){
+	//while(it != callGraph.end()){
+		
+	for(CallGraph::iterator it = callGraph.begin(); it!= callGraph.end(); ++it){
 		//we go through sets of methods of every scope
 		set<int> scopeMethods = it->second;
 		
@@ -133,77 +172,52 @@ int main (int argc, char* argv[]) {
 		
 		//calculate pair support within the scope in while loop, add it to the pair support table
 		for(set<int>::iterator it3 = scopeMethods.begin();it3!=scopeMethods.end(); ++it3){	//iterate over all methods	
-			for(set<int>::iterator it4 = scopeMethods.begin();it4!=scopeMethods.end(); ++it4){ //iterate over all methods again
-				if(*it3 == *it4){
-					continue; //except the one we already have in the loop one level up
+			for(set<int>::iterator it4 = /*scopeMethods.begin()*/it3;it4!=scopeMethods.end(); ++it4){ //iterate over all methods again
+				if(*it3 != *it4){//except the one we already have in the loop one level up
+					//printf("comparing %s and %s\n",hashIntToString(*it3).c_str(),hashIntToString(*it4).c_str());
+					pair<int,int> pair;			
+					if(*it3<*it4){
+						pair = make_pair(*it3,*it4);
+					}
+					else{
+						pair = make_pair(*it4,*it3);
+					}
+					pairSupport[pair]++;	
 				}	
-				pair<int,int> pair;			
-				if(*it3<*it4){
-					pair = make_pair(*it3,*it4);
-				}
-				else{
-					pair = make_pair(*it4,*it3);
-				}
-				pairSupport[pair]++;				
+							
 			}
 			
 			//std::cout << individualSupport[*it2];
-		}
-		
-		it++;
+		}		
 	}
 	
-	/*printf("printing the graph:\n");
-	
-	CallGraph::iterator it5 = callGraph.begin();
-	while(it5 != callGraph.end()){		//over every scope
-		set<int> scopeMethods2 = it5->second;
-		int scopeInteger = it5->first;
-		printf("Scope: %s\n",hashIntToString(scopeInteger).c_str());
-		//calculate pair support within the scope in while loop, add it to the pair support table
-		for(set<int>::iterator it7 = scopeMethods2.begin();it7!=scopeMethods2.end(); ++it7){	//iterate over all methods		
-			int method1 = *it7;
-			printf("method: %s\n", hashIntToString(method1).c_str());
-		}
-		
-		it5++;
-	}*/
-	
-	
-	
-	//analyze graph with our knowledge of support
-	CallGraph::iterator it5 = callGraph.begin();
-	while(it5 != callGraph.end()){		//over every scope
-		set<int> scopeMethods2 = it5->second;
-		int scopeInteger = it5->first;
-		//calculate pair support within the scope in while loop, add it to the pair support table
-		for(set<int>::iterator it7 = scopeMethods2.begin();it7!=scopeMethods2.end(); ++it7){	//iterate over all methods		
-			int method1 = *it7;
-			double methodConfidence;
-			for(set<int>::iterator it8 = scopeMethods2.begin();it8!=scopeMethods2.end(); ++it8){ //iterate over all methods again
-				if(*it7 == *it8){
-					continue; //except the one we already have in the loop one level up
-				}				
-				pair<int,int> pair;
-				if(*it7<*it8){
-					pair = make_pair(*it7,*it8);
-				}
-				else{
-					pair = make_pair(*it8,*it7);
-				}
-				methodConfidence = (pairSupport[pair] / individualSupport[*it7])*100;	
-				//std::cout << methodConfidence;
-				
-				if (methodConfidence >= confidence && pairSupport[pair] >= support){
-					printf("bug: %s in %s, pair: (%s, %s), support: %d, confidence: %.2f%%\n",hashIntToString(*it7).c_str(),hashIntToString(scopeInteger).c_str(),hashIntToString(*it7).c_str(),hashIntToString(*it8).c_str(),pairSupport[pair], methodConfidence);
-				}
-			}
+	for(PairSupport::iterator it5 = pairSupport.begin(); it5!= pairSupport.end(); ++it5){
+		pair<int,int> pair = it5->first;
+		int thisPairSupport = it5->second;
+		if (thisPairSupport >= support){
+			int firstMethod = pair.first;
+			int secondMethod = pair.second;
+			double firstConfidence = ((double)thisPairSupport/(double)individualSupport[firstMethod])*100.00;
+			//printf("thispairsupport: %.2f",(double)thisPairSupport);
+			//printf("/ individualsupport: %.2f",(double)individualSupport[firstMethod]);
 			
-			//std::cout << individualSupport[*it2];
+			
+			
+			double secondConfidence = ((double)thisPairSupport/(double)individualSupport[secondMethod])*100.00;
+			
+			//printf("firstConfidence: %f", firstConfidence);
+			//printf("secondConfidence: %f", secondConfidence);
+			//printf("confidence: %f", confidence);
+			
+			if(firstConfidence >= confidence){
+				lookForBugs(callGraph,firstMethod,secondMethod,thisPairSupport,firstConfidence);
+			}
+			if(secondConfidence >= confidence){
+				lookForBugs(callGraph,secondMethod,firstMethod,thisPairSupport,secondConfidence);
+			}
 		}
-		
-		it5++;
 	}
+	
 	
 		
 	return 0;
